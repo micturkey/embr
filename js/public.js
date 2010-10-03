295ffd53415d943c0071240d16afe12fadcea5e1
hjl250481892 <hjl250481892@gmail.com>
2010-10-03 12:07:51 +0000

    added: js/message.js fix: leaveWord in message.php

--- js/public.js
+++ js/public.js
@@ -9,6 +9,9 @@ function leaveWord(num) {
 		num = 140;
 	}
 	var leave = num-$("#textbox").val().length;
+	if ($("#sent_id").val()) {
+		leave -= $("#sent_id").val().length + 3;
+	}
 	if (leave < 0) {
 		$("#tip").css("color","#CC0000");
 		$("#tip b").css("color","#CC0000");
@@ -319,6 +322,10 @@ var formFunc = function(){
 							$('#tip').removeClass('loading');
 							if ( (text.substring(0,2)).toUpperCase() == "D "){ //exclude the DMs. the exam of user_name is omitted.
 								updateSentTip("Your DM has been sent!", 3000, "success");
+								$("#textbox").val("");
+								$("#sent_id").val("");
+								$("#tip").html("<b>140</b>");
+								leaveWord();
 							} else {
 							updateSentTip("Your status has been updated!", 3000, "success");
 							$("#textbox").val("");
@@ -974,43 +981,6 @@ var formFunc = function(){
 				}
 			});
 		});
-
-//message function
-$(function () {
-	$(".msg_delete_btn").click(function(e){
-		e.preventDefault();
-		
-		var $this = $(this);
-		var message_id = $.trim($this.parent().parent().find(".status_id").text());
-		var confirm = window.confirm("Are you sure to delete this message?");
-		
-		if (confirm) {
-			updateSentTip("Deleting message...", 5000, "ing");
-			$.ajax({
-				url: "ajax/delete.php",
-				type: "POST",
-				data: "message_id=" + message_id,
-				success: function(msg) {
-					if (msg.indexOf("success") >= 0) {
-						$this.parent().parent().parent().remove();
-						updateSentTip("Message deleted.", 3000, "success");
-					} else {
-						updateSentTip("Failed to delete this message!", 3000, "failure");
-					}
-				},
-				error: function(msg) {
-					updateSentTip("Failed to delete this message!", 3000, "failure");
-				}
-			});
-		}
-	});
-
-	$(".msg_replie_btn").click(function(e){
-		e.preventDefault();
-		$("#sent_id").val($(this).parent().parent().find(".status_word").find(".user_name").text());
-		$("#textbox").focus();
-	});
-});
 
 	//init global functions
 	$(document).ready(function () {
