c664ba1d453c6cb2543679eb66fd5e4cbc035126
hjl250481892@gmail.com <hjl250481892@gmail.com@91a06797-e009-8ff7-0b38-e8ee2ad1c0bf>
2011-03-20 05:26:35 +0000

    Fix: ISSUE 58 Deleted: Nexus_logo & intro

--- trunk/lib/config.php
+++ trunk/lib/config.php
@@ -1,15 +1,14 @@
 <?php
 	define('API_URL', 'http://api.twitter.com/1');
 	define('TWITESE_API_URL', 'http://twiteseapi.appspot.com');//need NOT change it
-	define('BASE_URL', 'http://embr.in'); //define base_url for share utility
 	
 	// Config
 	define('SECURE_KEY', 'YOU SHOULD CHANGE IT');
-	define('BASE_URL','http://embr.tk'); //Where your embr is hosted, i.e. http://bugbug.tk
+	define('BASE_URL','http://embr.in'); //Where your embr is hosted, i.e. http://bugbug.tk
 	define("CONSUMER_KEY", "TEItTaPASySnYxziOyIdag");
 	define("CONSUMER_SECRET", "xJEoWvBumpqgiiBuviWTa7GT8KCvP7Kv3n0hixhJaZY");
 	
 	//Optional Information
 	define('SITE_OWNER', 'TWITTER'); //Your Twitter ID  
-	define('BLOG_SITE','http://jlhwung.tk'); //blog_site
+	define('BLOG_SITE',''); //blog_site
 ?>
